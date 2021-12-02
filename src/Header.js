59c8cadecc56afc394af89145ee94ec2efcf37c0
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2021-12-01 20:03:35 -0500

    version1-portafolio

--- src/Header.js
+++ src/Header.js
@@ -5,19 +5,19 @@ function Header() {
     return (
         <div className="header">
         <div className="header__menu">
-            <a href="/" className="header__menu__logo"><img src={logo} alt="logo"/></a>
+            {/* <a href="/" className="header__menu__logo"><img src={logo} alt="logo"/></a> */}
 
             <div className="header__nav">
                 <nav>
 
                 <div class="wrapper">
-                    <button> Button
-                    </button>
+                    
                 </div>
-                <div><h2>cabecera</h2></div>
+                <div></div>
                     <a href="/contacto">Contacto</a>
                     <a href="/#proyectos">Proyecto</a>
                     <a href="https://github.com/FredyGoGa">Github</a>
+                    
                 </nav>
             </div>
         </div>
