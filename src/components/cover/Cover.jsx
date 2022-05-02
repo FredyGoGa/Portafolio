76fd1821251e865e2a9c35a53f3c6cea05097822
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2022-05-01 23:02:40 -0500

    componenete cover terminado

--- src/components/cover/Cover.jsx
+++ src/components/cover/Cover.jsx
@@ -6,6 +6,8 @@ const Cover = () => {
   return (
     <div className="cover-container">
       <video className="video" src={coverVideo} autoPlay loop muted />
+      <h1>Fredy Gonzalez Garcia</h1>
+      <p>React js Developer | ADSI technology | Content Creator | Entrepeneur </p>
     </div>
   )
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./navbar.css";
+
+const Navbar = ({ isScrolling }) => {
+  return (
+    <nav className="navbar">
+    <div className="navbar-logo">
+      Your name
+    </div>
+  </nav>
+  )
+}
+
+export default Navbar
